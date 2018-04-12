4c19dc174e26acff85448e8e89b80f2b3a719bcc
Nicholas Wease <skynet60@outlook.com>
2018-04-12 11:56:32 +0200

    Hardcoded test data into files

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
-/*  This is the home route. It renders the index.mustache page from the views directory.
-	Data is rendered using the Mustache templating engine. For more
-	information, view here: https://mustache.github.io/#demo */
-router.get('/', function(req, res){
-	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
+router.get('/', (req, res) => {
+	const apartments = [
+		{title: 'Detroit great find', numRooms: 4},
+		{title: 'Luxury apartment', numRooms: 5},
+		{title: '4 bedroom apartment', numRooms: 4}
+	]
+
+	res.render('index', {data: apartments})
 })
 
 /*  This route render json data */
