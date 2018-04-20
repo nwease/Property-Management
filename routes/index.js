82fe706225e299245509318fbb32c916dc5b14cd
Nicholas Wease <skynet60@outlook.com>
2018-04-20 20:31:17 +0200

    styling changes

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,31 @@
 // Full Documentation - https://www.turbo360.co/docs
-const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
-const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
-const router = vertex.router()
+const turbo = require("turbo360")({ site_id: process.env.TURBO_APP_ID });
+const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
+const router = vertex.router();
 
-router.get('/:name/:city/:state', (req, res) => {
-	turbo.create('building', {
-		name: req.params.name, 
-		city: req.params.city, 
-		state: req.params.state
-	}).then((data) => {
-		console.log(JSON.stringify(data))
-	}).catch((err) => {})
-
-	return
-})
+router.get("/", (req, res) => {
+  turbo.fetch("building", null).then(buildings => {
+    res.render("index", { data: buildings });
+  });
+});
 
 /*  This route render json data */
-router.get('/json', function(req, res){
-	res.json({
-		confirmation: 'success',
-		app: process.env.TURBO_APP_ID,
-		data: 'this is a sample json route.'
-	})
-})
+router.get("/json", function(req, res) {
+  res.json({
+    confirmation: "success",
+    app: process.env.TURBO_APP_ID,
+    data: "this is a sample json route."
+  });
+});
 
 /*  This route sends text back as plain text. */
-router.get('/send', function(req, res){
-	res.send('This is the Send Route')
-})
+router.get("/send", function(req, res) {
+  res.send("This is the Send Route");
+});
 
 /*  This route redirects requests to Turbo360. */
-router.get('/redirect', function(req, res){
-	res.redirect('https://www.turbo360.co/landing')
-})
+router.get("/redirect", function(req, res) {
+  res.redirect("https://www.turbo360.co/landing");
+});
 
-
-module.exports = router
+module.exports = router;
