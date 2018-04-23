0552d39a624d71246da20b880536d2a5c4f5568b
Nicholas Wease <skynet60@outlook.com>
2018-04-23 13:57:20 +0200

    setup dynamic data

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,50 @@
 // Full Documentation - https://www.turbo360.co/docs
-const turbo = require("turbo360")({ site_id: process.env.TURBO_APP_ID });
-const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
-const router = vertex.router();
+const turbo = require("turbo360")({ site_id: process.env.TURBO_APP_ID })
+const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID })
+const router = vertex.router()
 
 router.get("/", (req, res) => {
   turbo.fetch("building", null).then(buildings => {
-    res.render("index", { data: buildings });
-  });
-});
+    res.render("index", { data: buildings })
+  })
+})
+
+router.get("/:buildingSlug", (req, res) => {
+  let building = {}
+  turbo
+    .fetch("building", { slug: req.params.buildingSlug })
+    .then(buildings => {
+      building = buildings[0]
+      return turbo.fetch('apartment', {building: buildings[0].id})
+    })
+    .then((apartments) => {
+      console.log(JSON.stringify(apartments))
+      building.apartments = apartments
+      res.render('building', building)
+    })
+    .catch(err => {
+      console.log(err)
+      return
+    })
+})
+
+router.post('/:buildingSlug', (req, res) => {
+  let params = req.body
+
+  turbo
+    .fetch("building", { slug: req.params.buildingSlug })
+    .then(buildings => {
+      params.building = buildings[0].id
+      return turbo.create('apartment', params)
+    })
+    .then((apartment) => {
+      console.log(apartment)
+      res.redirect('/' + req.params.buildingSlug)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 /*  This route render json data */
 router.get("/json", function(req, res) {
@@ -15,17 +52,17 @@ router.get("/json", function(req, res) {
     confirmation: "success",
     app: process.env.TURBO_APP_ID,
     data: "this is a sample json route."
-  });
-});
+  })
+})
 
 /*  This route sends text back as plain text. */
 router.get("/send", function(req, res) {
-  res.send("This is the Send Route");
-});
+  res.send("This is the Send Route")
+})
 
 /*  This route redirects requests to Turbo360. */
 router.get("/redirect", function(req, res) {
-  res.redirect("https://www.turbo360.co/landing");
-});
+  res.redirect("https://www.turbo360.co/landing")
+})
 
-module.exports = router;
+module.exports = router
